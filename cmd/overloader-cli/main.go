c8d37a5457ab443a8e75fb44a901ec6c067269a1
asajaroff <asajaroff@users.noreply.github.com>
2021-04-25 10:50:17 -0300

    WIP - Attempting to add concurrency #1

--- cmd/overloader-cli/main.go
+++ cmd/overloader-cli/main.go
@@ -5,38 +5,83 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-
-	//"net/http"
-
+	"sync"
 	//"time"
-	//"fmt"
-	toml "github.com/pelletier/go-toml"
 )
 
-var (
-	WarningLogger *log.Logger
-	InfoLogger    *log.Logger
-	ErrorLogger   *log.Logger
-)
+// handleRequests recives the URL, method and paylod (if request =! GET)
+// and returns the HTTP response status code
+func handleRequest(target string, method string) {
+	switch method {
+	case "GET":
+		log.Println("Sending ", method, "requests to target (", target, ")")
+		response, err := http.Get(target)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Println(response.StatusCode)
+		log.Print(response.Body)
+
+		bytes, errRead := ioutil.ReadAll(response.Body)
+		if errRead != nil {
+			log.Fatal(errRead)
+		}
+		log.Println("Response payload: ", len(string(bytes)), " bytes.")
+		return
+
+	case "HEAD":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "POST":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "PUT":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "DELETE":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "CONNECT":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "OPTIONS":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "TRACE":
+		// ToDo
+		log.Println("Implementation missing")
+
+	case "PATCH":
+		// ToDo
+		log.Println("Implementation missing")
+	}
+}
+
+const count = 500
+const url = "https://www.google.com"
 
 func main() {
-	config, err := toml.LoadFile("config.toml")
+	wg := sync.WaitGroup{}
 
-	if err != nil {
-		fmt.Println("Error ", err.Error())
-	} else {
-		target := "https://" + config.Get("target.domain").(string)
-		log.Println("Target present in config.toml: ", target)
-		resp, err := http.Get(target)
-		if err != nil {
-			log.Println("Error requesting ", target, ": ", err)
-		}
-		body, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			fmt.Println("Error: ", err)
-		}
-		fmt.Print(body)
-		defer resp.Body.Close()
-		// ...
+	for id := 0; id <= count; id++ {
+		wg.Add(1)
+		go func(id int) {
+			// response, err := handleRequest(url, "GET")
+			// if err != nil {
+			// 	return
+			// }
+			handleRequest(url, "GET")
+			fmt.Println("Request ", id)
+
+			wg.Done()
+
+		}(id)
 	}
 }
